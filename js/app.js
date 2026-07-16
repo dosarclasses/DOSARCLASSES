7d902532ddacec0dffbe5bb112ab7769f1c099e1
dosarclasses <dosarclasses@gmail.com>
2026-07-16 22:35:51 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// DOSAR Classes JavaScript
+const observer=new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll(".fade-up").forEach(el=>observer.observe(el));
